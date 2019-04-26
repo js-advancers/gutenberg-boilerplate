a9d3f9a5b7477e84bd76694abb71ef0f39b0b8e5
Fabian Kägy <fabian@arvernus.info>
2019-04-26 09:09:20 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,73 +13,9 @@ function recursiveIssuer( m ) {
 }
 
 module.exports = {
-	...defaultConfig,
-	optimization: {
-		splitChunks: {
-			chunks: 'all',
-			cacheGroups: {
-				editor: {
-					name: 'editor',
-					test: /editor\.(sc|sa|c)ss$/,
-					enforce: true,
-				},
-				style: {
-					name: 'styles',
-					test: /style\.(sc|sa|c)ss$/,
-					enforce: true,
-				},
-				theme: {
-					name: 'theme',
-					test: /theme\.(sc|sa|c)ss$/,
-					enforce: true,
-				},
-				default: false,
-			},
-		},
-	},
-	module: {
-		...defaultConfig.module,
-		rules: [
-			...defaultConfig.module.rules,
-			{
-				test: /\.(sc|sa|c)ss$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-					},
-					{
-						loader: require.resolve( 'css-loader' ),
-					},
-					{
-						loader: require.resolve( 'sass-loader' ),
-					},
-					{
-						loader: require.resolve( 'postcss-loader' ),
-						options: {
-							ident: 'postcss',
-							plugins: () => [ postcssPresetEnv({
-                // Start with a stage.
-                stage: 3,
-                // Override specific features you do (or don't) want.
-                features: {
-                  // And, optionally, configure rules/features as needed.
-                  'custom-media-queries': {
-                    preserve: false,
-                  },
-                  'custom-properties': {
-                    preserve: true,
-                  },
-                  'nesting-rules': true,
-                },
-              } ) ],
-						},
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		...defaultConfig.plugins,
-		new MiniCssExtractPlugin(),
-	],
+	
+	entry: {
+		...defaultConfig.entry,
+		frontend: './src/frontend.js',
+	}
 };
