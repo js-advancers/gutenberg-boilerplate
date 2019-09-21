8339fe78230a1a875a4faa9ed66e00d8ee20446d
Fabian Kägy <mail@fabian-kaegy.de>
2019-09-21 22:10:25 +0200

    hoc-example

---
+++ src/blocks/data-example/components/toggleSidebarButton.js
@@ -0,0 +1,35 @@
+import { Button } from '@wordpress/components';
+import { withSelect, withDispatch } from '@wordpress/data';
+import { compose } from '@wordpress/compose';
+
+const ToggleSidebarButton = ( {
+	isEditorSidebarOpened,
+	openGeneralSidebar,
+} ) => {
+	if ( isEditorSidebarOpened ) {
+		return null;
+	}
+	return (
+		<Button className="is-button is-primary" onClick={ openGeneralSidebar }>
+            Open Block Settings
+		</Button>
+	);
+};
+
+export default compose(
+	withSelect( ( select ) => {
+		return {
+			isEditorSidebarOpened: select( 'core/edit-post' ).isEditorSidebarOpened(),
+		};
+	} ),
+	withDispatch( ( dispatch, ownProps, { select } ) => {
+		const { getBlockSelectionStart } = select( 'core/editor' );
+		const { openGeneralSidebar, closeGeneralSidebar } = dispatch( 'core/edit-post' );
+
+		return {
+			openGeneralSidebar: () =>
+				openGeneralSidebar( getBlockSelectionStart() ? 'edit-post/block' : 'edit-post/document' ),
+			closeGeneralSidebar,
+		};
+	} )
+)( ToggleSidebarButton );
--- src/blocks/data-example/edit.js
+++ src/blocks/data-example/edit.js
@@ -1,25 +1,10 @@
-import { useSelect, useDispatch } from '@wordpress/data';
+import ToggleSidebarButton from './components/toggleSidebarButton';
 
 const edit = ( props ) => {
 	const { attributes: { }, setAttributes } = props;
 
-	const isSidebarOpened = useSelect( ( select ) => ( select( 'core/edit-post' ).isEditorSidebarOpened() ) );
-	const dispachStuff = useDispatch( ( dispatch ) => {
-		debugger;
-		const { getBlockSelectionStart } = select( 'core/editor' );
-		const { openGeneralSidebar, closeGeneralSidebar } = dispatch( 'core/edit-post' );
-
-		return {
-			openSidebar: () =>
-				openGeneralSidebar( getBlockSelectionStart() ? 'edit-post/block' : 'edit-post/document' ),
-			closeSidebar: () => closeGeneralSidebar,
-		};
-	} );
-
-	console.log( isSidebarOpened );
-	console.log( dispachStuff );
 	return (
-		<p>Hello World!</p>
+		<p>Hello World!<ToggleSidebarButton /></p>
 	);
 };
 
